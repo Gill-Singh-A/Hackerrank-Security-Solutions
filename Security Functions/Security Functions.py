c802284452bcec3580d13e4e6621d1e1e86bf133
Gill-Singh-A <amansg22@iitk.ac.in>
2023-05-03 01:36:15 +0530

    Added Security Functions

---
+++ Security Functions/Security Functions.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def calculate(x):
+    return x % 11
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    x = int(input().strip())
+
+    result = calculate(x)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
